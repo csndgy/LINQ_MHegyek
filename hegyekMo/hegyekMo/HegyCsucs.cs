e5d88e62f0b3a14bdb44de2440742d7d1441fa95
csndgy <73069087+csndgy@users.noreply.github.com>
2023-05-18 14:21:38 +0200

    Add files via upload

---
+++ hegyekMo/hegyekMo/HegyCsucs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hegyekMo
+{
+    internal class HegyCsucs
+    {
+        public string hegycsucs { get; set; }
+        public string hegyegE { get; set; }
+        public double magassag { get; set; }
+
+        public HegyCsucs(string hegycsucs, string hegysegE, int magassag)
+        {
+            this.hegycsucs = hegycsucs;
+            this.hegyegE = hegyegE;
+            this.magassag = magassag;
+        }
+    }
+    
+}
---
+++ hegyekMo/hegyekMo/Program.cs
@@ -0,0 +1,38 @@
+﻿using hegyekMo;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<HegyCsucs> csucsok = new List<HegyCsucs>();
+            string adatok = "hegyekMo.txt";
+            StreamReader sr = new StreamReader(adatok);
+            //sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                string[] adat = sr.ReadLine().Split(';');
+                csucsok.Add(new HegyCsucs(adat[0], adat[1], int.Parse(adat[2])));
+            }
+            sr.Close();
+
+            double osszeg = 0;
+
+            foreach (HegyCsucs vmi in csucsok)
+            {
+                osszeg = osszeg + vmi.magassag ;
+            }
+
+
+            int megszamla = csucsok.Count;
+            double atlagmagassag = osszeg / megszamla;
+            Console.WriteLine($"{megszamla} hegy található az állományban.");
+            Console.WriteLine($"{atlagmagassag} az átlagmagasság.");
+
+        }
+    }
+}
